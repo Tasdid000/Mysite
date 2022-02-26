0e4f3f82484d567344a83c9a43def8e7147bba38
Siam000 <tasdidhasnain036@gmail.com>
2022-02-26 10:06:25 +0600

    first commit

--- home/views.py
+++ home/views.py
@@ -1,9 +1,9 @@
 #from django.db.models import query
 from django.shortcuts import render, HttpResponse, redirect
-#from django.contrib import messages
+from django.contrib import messages
 #from django.contrib.auth import authenticate, login, logout
 #from django.contrib.auth.models import User
-#from home.models import Contact
+from home.models import Contact
 #from blog.models import Post
 
 def home(request):
@@ -11,7 +11,22 @@ def home(request):
 def about(request):
     return render(request, "home/about.html")
 def contact(request):
-    return render(request, "home/contact.html")
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name, email, phone, content)
+        if len(name) < 2 or len(email) < 4 or len(phone) < 10 or len(content) < 3:
+            messages.error(request, "Please fill the form correctly")
+
+        else:
+            contact = Contact(name=name, email=email, phone=phone, content=content)
+            contact.save()
+            messages.success(request, "Your message has been successfully sent")
+
+    return render(request, "home/home.html")
+
 def join(request):
     return render(request, "home/join.html")
 
